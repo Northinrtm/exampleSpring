0b7b2501d06015f2c10946f067972c6be61ea0c8
northinrtm <artsdurden@gmail.com>
2022-11-23 01:23:05 +0300

    все работает, но не выводит внесенные ранее обьекты

--- src/main/java/com/example/examplespring/service/StoreService.java
+++ src/main/java/com/example/examplespring/service/StoreService.java
@@ -1,15 +1,17 @@
 package com.example.examplespring.service;
 
 import com.example.examplespring.model.Product;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@Scope("prototype")
+@SessionScope
 public class StoreService {
     private final Map<Integer, Product> productMap = new HashMap<>();
 
